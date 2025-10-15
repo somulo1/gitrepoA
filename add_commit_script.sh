137569481448f5a6dc93cf64e396ff224abc2e6e
somulo <mcomulosammy37@gmail.com>
2025-10-15 10:38:33 +0300

    add env files

--- add_commit_script.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Script to add and commit each untracked file one by one
-
-git ls-files --others --exclude-standard | while read -r file; do
-    git add "$file"
-    git commit -m "Add $file"
-done
